e797120bb5df39ecfd7c8f0c2f957a5b5fe99f49
aarafathh <aarafathh@gmail.com>
2026-04-05 16:08:22 +0600

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Arafath Hossain on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
