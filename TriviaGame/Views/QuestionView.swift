75547b8dd57192c6107a38155a76a7a9501a5d2b
aarafathh <aarafathh@gmail.com>
2026-04-12 19:57:28 +0600

    Trivia Game Completed

---
+++ TriviaGame/Views/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  TriviaGame
+//
+//  Created by Arafath Hossain on 5/4/26.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionView()
+}
